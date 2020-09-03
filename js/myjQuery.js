1db7aae6391ca211f92a8ad9a780639d153bf20e
minyongsim <alsdyd5411@naver.com>
2020-09-03 21:02:08 +0900

    스크롤 이벤트 오류

--- js/myjQuery.js
+++ js/myjQuery.js
@@ -129,8 +129,81 @@ $(function($){
     }
     changeList(lieq)
     })
- 
+    
+    var sct=0;
+	$(window).scroll(function(){
+		sct=$(this).scrollTop();
+		if(sct>=winHeight){
+            $(".header-outer").css({
+                background:'rgba(0,0,0,1)'
+            });
+            
+		} else {
+            $(".header-outer").css({
+                background:'rgba(0,0,0,0.5)'
+            });
+        }
 
+        // 스크롤탑값이 100이상이 되면 맨위로 버튼이 보이고 100미만이면 숨기기
+        if(sct>=100){
+            $(".gotop").addClass("on").stop().animate({
+                opacity:"1"
+            },500)
+        }else{
+            $(".gotop").removeClass("on").stop().animate({
+                opacity:"0"
+            },500)
+        }
+        // sct 값이 # 
+        if(sct >= $("#skills").offset().top) {
+            $(".skilcontainer").stop().fadeIn(300)
+        }else{
+            $(".skilcontainer").hide()
+        }
+    });
+    $(".gotop").on("click",function(){
+        $("html,body").stop().animate({
+            scrollTop:"0"
+        },800,"linear")
+    })
+    
+    $('.nav .depth1 > li > a').on('click', function(e){
+        e.preventDefault();
+        $(this).parent().addClass('on')
+        $(this).parent().siblings().removeClass('on')
+        var index = $(this).parent().index()
+        $('body, html').stop().animate({
+            scrollTop:index*winHeight
+        }, 800)
+        //return false
+ 
+    })
+
+    $(".section").on("mousewheel",function(e, wh){    
+        var index = $(this).index()
+		//마우스 휠을 올렸을때	
+          if (wh > 0) {  
+			//변수 prev에 현재 휠을 움직인 section에서 이전 section의 offset().top위치 저장
+             var prev = $(this).prev().offset().top;
+             $('.depth1 li').eq(index-1).addClass('on')
+             $('.depth1 li').eq(index-1).siblings().removeClass('on')
+			 //문서 전체를 prev에 저장된 위치로 이동
+			 $("html,body").stop().animate({
+                 scrollTop:prev
+                },800,"linear");
+		//마우스 휠을 내렸을때	 
+          }else if (wh < 0) {  
+			//변수 next에 현재 휠을 움직인 section에서 다음 section의 offset().top위치 저장
+             var next = $(this).next().offset().top;
+             $('.depth1 li').eq(index+1).addClass('on')
+            $('.depth1 li').eq(index+1).siblings().removeClass('on')
+			 //문서 전체를 next에 저장된 위치로 이동
+			 $("html,body").stop().animate({
+                 scrollTop:next
+                },800,"linear");                                         
+          }
+        
+     });
 
 
  
